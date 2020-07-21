ac977ff88c66d491b84631f0ce49e23e4fbebc3c
jhye0n <pwn3rhyun@naver.com>
2020-07-21 13:09:08 +0900

    review

--- main.cpp
+++ main.cpp
@@ -9,33 +9,33 @@ uint32_t gilgil(uint32_t n){
 }
 
 int main(int argc, char *argv[]){
-	FILE *fd, *fd2;
-	uint32_t buf, buf2, sum;
+	FILE *fd, *fd2; // file pointer defined
+	uint32_t buf, buf2, sum; // uint32_t type variable defined
 
-	if(argc < 2){
+	if(argc < 2){ // !argc(argv[1], argv[2] -> error occured
 		printf("argument error %s <file1><file2>\n",argv[0]);
 		return 0;
 	}
 
-	fd = fopen(argv[1],"r");
+	fd = fopen(argv[1],"r"); // fopen function(argv[1], ex, thousand.bin
 
 	if(fd == NULL){
 		printf("file open error");
 		return 0;
 	}
 
-	fd2 = fopen(argv[2], "r");
+	fd2 = fopen(argv[2], "r"); // fopen function(argv[2] ex, five-hundred.bin
 
 	if(fd2 == NULL){
 		printf("file open error");
 		return 0;
 	}
 
-	fread(&buf, 1, 256, fd);
-	fread(&buf2, 1, 256, fd2);
+	fread(&buf, 1, sizeof(&buf), fd); // buf sizeof added
+	fread(&buf2, 1, sizeof(&buf), fd2); // buf sizeof added
 
-	buf = gilgil(buf);
-	buf2 = gilgil(buf2);
+	buf = gilgil(buf); // uint32_t gilgil function call
+	buf2 = gilgil(buf2); // uint32_t gilgil function call
 	sum = buf+buf2;
 
 	printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n",buf,buf,buf2,buf2,sum,sum);
